8d5f15108dbb4b689defc8601f6b72a023621fe3
kiko <kikitosgon@gmail.com>
2022-04-14 16:26:22 +0100

    novos testes

---
+++ ola.py
@@ -0,0 +1,21 @@
+
+
+
+
+# from ast import While
+# from cgi import print_arguments
+# from re import S
+
+print("olá senhor.")
+print("escreva o seu nome aqui em baixo")  
+nome=input("qual é o seu nome:")
+print("olá senhor", nome ,"seja bem vindo" )
+resposta = "s"
+
+while resposta == "s" :
+    numero1 = input ("escreva um numero:")
+    numero2 = input ("escreva outro numero:")
+    numero3 = print ("a soma entre os dois numeros que escolheu é:" , int(numero2) + int(numero1) )
+    resposta = input("queres fazer outra conta?")
+ 
+print("adeus!")
