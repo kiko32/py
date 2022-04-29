08c6d46319ce623f6f5cf25bc74007581b8480e6
kiko <kikitosgon@gmail.com>
2022-04-29 17:04:05 +0100

    alarme/relogio/cronómetro

--- relogio.py
+++ relogio.py
@@ -2,31 +2,31 @@
 
 
 from ast import While
+
 from time import sleep
 
+
 horas = 0
-
 minutos = 0
-
 segundos = 0
 
-while horas < 24 :
+minutos = int(input("diga os minutos:"))
+horas = int(input("diga nas horas:"))
+
+horasAlarme = int(input("para que horas quere o alarm:"))
+minutosAlarme = int(input("para que minuto quere o alarm:"))
+
+while horas < 24 : 
     while minutos < 60 :
+        if horasAlarme == horas :
+            if minutosAlarme == minutos :
+                print("acabou o tempo!!!")
+                exit()
         while segundos < 60 :
             print (horas, ":", minutos, ":", segundos)
             segundos = segundos + 1
-            sleep(1)
+            sleep(0.25)
         segundos = 0
         minutos = minutos + 1
     horas = horas + 1
-    minutos = 0
-
-    
-    
-
-# while minutos < 60 :
-#     while segundos < 60 :
-#         print(minutos, ":", segundos)
-#         segundos = segundos + 1
-#     minutos = minutos + 1
-#     segundos = 0
+    minutos = 0    
